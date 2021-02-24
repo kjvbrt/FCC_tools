fdcd174d2970cbf7d5ad18ab6a6b8eacf5bbc35c
Juraj Smiesko <juraj.smiesko@cern.ch>
2021-02-24 10:14:42 +0100

    Allowing any number of parameters

--- calo_energy_corr/utils.py
+++ calo_energy_corr/utils.py
@@ -13,43 +13,24 @@ def count_bins_with_error(hist):
 
 
 def plot(obj, plot_name, plot_notes=[]):
-
-    canvas = TCanvas("canvas", "Canvas", 450, 450)
+    canvas = TCanvas('canvas' + plot_name, "Canvas", 450, 450)
     gPad.SetLeftMargin(.13)
     gPad.SetTopMargin(.05)
 
+    gStyle.SetOptStat(11)
+    gStyle.SetOptFit(1111)
+
     if 'TH2' in obj.ClassName():
         gPad.SetRightMargin(.13)
-
-        gStyle.SetOptStat(1111)
-        gStyle.SetOptFit(1111)
-        gStyle.SetStatX(0.5)
-        gStyle.SetStatW(0.2)
-        # gStyle.SetStatY(0.5)
-        # gStyle.SetStatH(0.2)
-
         draw_options = 'COLZ'
 
     if 'profile' in obj.GetName():
         gPad.SetRightMargin(.05)
-
-        gStyle.SetOptStat(11)
-        gStyle.SetOptFit(1111)
-        gStyle.SetStatX(0.5)
-        gStyle.SetStatW(0.2)
-
         draw_options = ''
 
     if 'graph' in obj.GetName():
         gPad.SetRightMargin(.05)
-
-        gStyle.SetOptStat(1111)
-        gStyle.SetOptFit(1111)
-        gStyle.SetStatY(0.6)
-        gStyle.SetStatH(0.2)
-
         draw_options = 'APE'
-
         obj.SetMarkerSize(.7)
         obj.SetMarkerStyle(21)
 
